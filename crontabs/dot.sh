69fb2de3107519a6e0cd0971320ebe138d5cba22
Ugase <5bgdbdkuhskiy@gmail.com>
2024-12-15 18:27:05 +0200

    M  crontabs/dot.sh

--- crontabs/dot.sh
+++ crontabs/dot.sh
@@ -1,12 +1,17 @@
-ping -c 1 github.com
-INTERNET=$?
-cd dotfiles
-stow --adopt --no-folding .
-git add . && git commit -m "$(git status --short)"
-
-if [[ $INTERNET -eq 0 ]]
+if [[ -n "$(git status --short)" ]]
 then
-  git push origin master
+  ping -c 1 github.com
+  INTERNET=$?
+  cd dotfiles
+  stow --adopt --no-folding .
+  git add . && git commit -m "$(git status --short)"
+  
+  if [[ $INTERNET -eq 0 ]]
+  then
+    git push origin master
+  else
+    printf "\033[31mNo Internet\033[0m"
+  fi
 else
-  printf "\033[31mNo Internet\033[0m"
+  printf "nothing to do"
 fi
