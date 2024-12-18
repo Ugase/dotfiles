f65b91fe4d809b308e563f685d2651e37aa18526
Ugase <5bgdbdkuhskiy@gmail.com>
2024-12-18 14:57:29 +0200

    M  .config/nvim/lua/config/lualine.lua

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -18,7 +18,23 @@ require("lualine").setup({
   sections = {
     lualine_a = { "mode" },
     lualine_b = { "filetype" },
-    lualine_c = { "filename", "diagnostics" },
+    lualine_c = {
+      {
+        "filename",
+        file_status = true, -- Displays file status (readonly status, modified status)
+        newfile_status = false, -- Display new file status (new file means no write after created)
+        path = 3, -- 0: Just the filename
+        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+        -- for other components. (terrible name, any suggestions?)
+        symbols = {
+          modified = "[+]", -- Text to show when the file is modified.
+          readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+          unnamed = "[No Name]", -- Text to show for unnamed buffers.
+          newfile = "[New]",
+        },
+      },
+      "diagnostics",
+    },
     lualine_x = { "os.date('%I:%M:%S %p')" },
     lualine_y = { "branch" },
     lualine_z = { "progress" },
