42516e767a02138f57c95c0fe586f86103a759fb
Ugase <5bgdbdkuhskiy@gmail.com>
2024-12-20 21:19:43 +0200

    A  .config/nvim/lua/plugins/disable.lua A  .config/nvim/lua/plugins/nvim-tree.lua M  crontabs/dot.sh

--- crontabs/dot.sh
+++ crontabs/dot.sh
@@ -1,42 +1,36 @@
-if [[ $1 != "--debug" ]]
-then
-  if [[ -n "$(git status --short)" ]]
-  then
+if [[ $1 != "--debug" ]]; then
+  if [[ -n "$(git status --short)" ]]; then
     ping -c 1 github.com
     INTERNET=$?
     cd ~/dotfiles
     stow --adopt --no-folding .
     git add . && git commit -m "$(git status --short)"
-    
-    if [[ $INTERNET -eq 0 ]]
-    then
+    if [[ $INTERNET -eq 0 ]]; then
       git push origin master
     else
       printf "\033[33mWarning: no internet connection avalible\n\033[0m"
     fi
-    else
-      printf "nothing to do\n"
-    fi
+  else
+    printf "nothing to do\n"
+  fi
 else
-  if [[ -n "$(git status --short)" ]]
-  then
+  if [[ -n "$(git status --short)" ]]; then
     FILE_PATH="$HOME/logs/$(date +%s).log"
     touch "${FILE_PATH}"
-    ping -c 1 github.com >> "${FILE_PATH}"
+    echo "Made ${FILE_PATH}"
+    ping -c 1 github.com >>"${FILE_PATH}"
     INTERNET=$?
-    echo "${INTERNET}" >> "${FILE_PATH}"
+    echo "${INTERNET}" >>"${FILE_PATH}"
     cd ~/dotfiles
-    echo "changed directory to ~/dotfiles" >> $FILE_PATH
-    stow --adopt --no-folding . >> $FILE_PATH
-    git add . >> $FILE_PATH && git commit -m "$(git status --short)" >> $FILE_PATH
-    
-    if [[ $INTERNET -eq 0 ]]
-    then
-      git push origin master >> $FILE_PATH 2>&1
+    echo "changed directory to ~/dotfiles" >>$FILE_PATH
+    stow --adopt --no-folding . >>$FILE_PATH
+    git add . >>$FILE_PATH && git commit -m "$(git status --short)" >>$FILE_PATH
+    if [[ $INTERNET -eq 0 ]]; then
+      git push origin master >>$FILE_PATH 2>&1
     else
-      printf "\033[33mWarning: no internet connection avalible\n\033[0m" >> $FILE_PATH
+      printf "\033[33mWarning: no internet connection avalible\n\033[0m" >>$FILE_PATH
     fi
-    else
-      printf "nothing to do\n" >> $FILE_PATH
-    fi
+  else
+    printf "nothing to do\n" >>$FILE_PATH
+  fi
 fi
