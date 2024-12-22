f5638e3e6de8b3e1d07cb4e624fc701292884050
Ugase <5bgdbdkuhskiy@gmail.com>
2024-12-22 10:04:42 +0200

    A  .config/nvim/lua/plugins/editing.lua R  .config/nvim/lua/plugins/nvim_tree.lua -> .config/nvim/lua/plugins/filetree.lua D  .config/nvim/lua/plugins/rainbow.lua D  .config/nvim/lua/plugins/tabline.lua D  .config/nvim/lua/plugins/trouble.lua

---
+++ .config/nvim/lua/plugins/editing.lua
@@ -0,0 +1,54 @@
+return {
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    },
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        lazy = true,
+        event = "BufAdd",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("bufferline").setup {}
+        end
+    },
+    {
+        "HiPhish/rainbow-delimiters.nvim",
+        lazy = true,
+        event = "BufReadPre"
+    }
+}
--- .config/nvim/lua/plugins/filetree.lua
+++ .config/nvim/lua/plugins/filetree.lua
@@ -10,3 +10,4 @@ return {
         require("nvim-tree").setup {}
     end,
 }
+
