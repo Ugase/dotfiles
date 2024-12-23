491a271f134fa5c06b1b9fbb378b70ed21353e09
Ugase <5bgdbdkuhskiy@gmail.com>
2024-12-23 11:15:47 +0200

    M  .config/nvim/lazy-lock.json M  .config/nvim/lua/config/themes.lua M  .config/nvim/lua/plugins/completion.lua

--- .config/nvim/lua/config/themes.lua
+++ .config/nvim/lua/config/themes.lua
@@ -4,5 +4,10 @@ return {
     {
         "samharju/synthweave.nvim",
         branch = "master"
-    }
+    },
+    {
+        "loctvl842/monokai-pro.nvim",
+        branch = "master"
+    },
+    "projekt0n/github-nvim-theme"
 }
--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -16,7 +16,7 @@ return {
             }
         },
         completion = {
-            ghost_text = { enabled = true },
+            -- ghost_text = { enabled = true },
             documentation = {
                 auto_show = true,
                 auto_show_delay_ms = 500,
