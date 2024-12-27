cb17fdb3757698cf88f933b829999f56d1a0de84
Ugase <5bgdbdkuhskiy@gmail.com>
2024-12-27 19:04:25 +0200

    M  .config/nvim/lua/plugins/ui.lua

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       require("mini.starter").setup({
         query_updaters = "",
-        footer = "",
+        footer = "\n",
         header = "\n                                    oo            \n                                                  \n88d888b. .d8888b. .d8888b. dP   .dP dP 88d8b.d8b. \n88'  `88 88ooood8 88'  `88 88   d8' 88 88'`88'`88 \n88    88 88.  ... 88.  .88 88 .88'  88 88  88  88 \ndP    dP `88888P' `88888P' 8888P'   dP dP  dP  dP \n                                                  \n",
       })
     end,
